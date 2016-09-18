f8f14139a06d46dd7abf03b5f92a20bf616c74f1
Recker Swartz <pk176543@hotmail.com>
2016-09-18 07:27:42 +0000

    paperclip working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, 
-  :default_url => "default.jpg",
-  :path => ":rails_root/public/controllers/:style/:basename.:extension",
-  :url => "/images/:id/:style/:basename.jpg"
+  default_url:  "default.jpg",
+  url: "/assets/products/:id/:attachment/:style/:basename.:extension",
+  path: ":rails_root/public/assets/products/:id/:attachment/:style/:basename.:extension"
+  validates_attachment_presence :avatar
+  validates_attachment_size :avatar, less_than: 5.megabytes
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :avatar,presence: true       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917075510) do
+ActiveRecord::Schema.define(version: 20160918044327) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at",                          null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160917075510) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
